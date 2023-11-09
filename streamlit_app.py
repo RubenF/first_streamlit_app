24dbdb9e97cdb111885d56a2671758c87eab7355
Rubén Flecha <ruben.flecha@gmail.com>
2023-11-09 17:31:53 -0300

    Update streamlit_app.py
    Added a header
    Added 3 lines of text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menú')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, spinach & Rockets')
+streamlit.text('Hard-boiled Free-Range Egg')
